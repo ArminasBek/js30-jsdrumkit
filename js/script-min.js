c9690f5d5a11d810374c3d77bc5a953cce5a8e80
Arminas <arminasb7@gmail.com>
2017-05-21 17:04:05 +0100

    initial commit

---
+++ js/script-min.js
@@ -0,0 +1,12 @@
+window.addEventListener("keydown", function(e){
+	//console.log(e.key);
+	var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);  
+	if(!audio) return // stop the function from running if key is not assigned
+	audio.currentTime = 0; //rewind the start.
+	audio.play();
+	key.classList.add('playing');
+});
+
+var keys = document.querySelectorAll('.key');
+
